3125cf13be3e77d246efa67ec6b99b2dcbc20de9
Jonathan Tan <jon.twk@gmail.com>
2018-09-09 16:09:10 -0500

    Update riddler-2018-09-07.py
    Performance optimizations: sets instead of lists at line 22, pre-allocated list at line 38, added time counter

--- code/riddler-2018-09-07.py
+++ code/riddler-2018-09-07.py
@@ -7,45 +7,45 @@
 # how long until you get all 100 cards?
 
 import random
-import statistics
+import time
 
-def buy_pack(num_cards, card_list):
+def buy_pack(num_cards, card_set):
     # select 10 random cards without duplication
     pack_cards = random.sample([x for x in range(1, num_cards + 1)], k = 10)
 
-    # print(f"Bought: {pack_cards}")
-
-    # change purchased card numbers to 0
+    # remove cards in pack from set once collected
     for card in pack_cards:
-        if card in card_list:
-            card_list[card - 1] = 0
+        if card in card_set:
+            card_set.remove(card)
 
 def collect_cards(num_cards):
-    # initialize 100 unique cards
-    cards_left = [x for x in range(1, num_cards + 1)]
-    num_packs = 0
+    # initialize 100 unique cards using a set
+    cards_left = set([x for x in range(1, num_cards + 1)])
 
     # loop until 0 cards are left
-    while sum(cards_left) > 0:
+    num_packs = 0
+
+    while cards_left:
         buy_pack(num_cards, cards_left)
         num_packs += 1
 
     return num_packs
 
-# loop over 1000 card runs
-num_simulations = 1000
+# loop over n card runs
+num_simulations = 10000
 num_cards = 100
 
-card_runs = []
+# pre-allocate list
+card_runs = [None] * num_simulations
 
 for i in range(num_simulations):
-    card_runs.append(collect_cards(num_cards))
+   card_runs[i] = collect_cards(num_cards)
 
-mean_packs = statistics.mean(card_runs)
-sd_packs = statistics.stdev(card_runs)
-
-print(f"After {num_simulations} simulations, all {num_cards} cards were collected in {mean_packs} +/- {sd_packs} packs.")
+# get mean number of packs needed to collect all cards
+mean_packs = sum(card_runs) / num_simulations
 
 num_weeks = mean_packs/10
 
+print(f"After {num_simulations} simulations, all {num_cards} cards were collected in {mean_packs} packs.")
 print(f"That's {num_weeks} weeks!")
+print(f"Time taken: {time.perf_counter()} s")
