894b25034290a2148b41866098269ac10bc80161
Jonathan Tan <jon.twk@gmail.com>
2018-09-09 14:38:44 -0500

    Update riddler-2018-09-07.py
    Removed hard-coding of number of cards to collect

--- code/riddler-2018-09-07.py
+++ code/riddler-2018-09-07.py
@@ -9,9 +9,9 @@
 import random
 import statistics
 
-def buy_pack(card_list):
+def buy_pack(num_cards, card_list):
     # select 10 random cards without duplication
-    pack_cards = random.sample([x for x in range(1, 101)], k = 10)
+    pack_cards = random.sample([x for x in range(1, num_cards + 1)], k = 10)
 
     # print(f"Bought: {pack_cards}")
 
@@ -20,30 +20,31 @@ def buy_pack(card_list):
         if card in card_list:
             card_list[card - 1] = 0
 
-def collect_cards():
+def collect_cards(num_cards):
     # initialize 100 unique cards
-    cards_left = [x for x in range(1, 101)]
+    cards_left = [x for x in range(1, num_cards + 1)]
     num_packs = 0
 
     # loop until 0 cards are left
     while sum(cards_left) > 0:
-        buy_pack(cards_left)
+        buy_pack(num_cards, cards_left)
         num_packs += 1
 
     return num_packs
 
-# loop over 10,000 card runs
-num_simulations = 10000
+# loop over 1000 card runs
+num_simulations = 1000
+num_cards = 100
 
 card_runs = []
 
 for i in range(num_simulations):
-    card_runs.append(collect_cards())
+    card_runs.append(collect_cards(num_cards))
 
 mean_packs = statistics.mean(card_runs)
 sd_packs = statistics.stdev(card_runs)
 
-print(f"After {num_simulations} simulations, all 100 cards were collected in {mean_packs} +/- {sd_packs} packs.")
+print(f"After {num_simulations} simulations, all {num_cards} cards were collected in {mean_packs} +/- {sd_packs} packs.")
 
 num_weeks = mean_packs/10
 
