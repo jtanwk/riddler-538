c9e96d8b916955b7d1acc42b6a759e3699d08be6
Jonathan Tan <jon.twk@gmail.com>
2019-07-23 00:59:43 -0400

    new week

---
+++ code/2018-11-30/riddler-2018-11-30.py
@@ -0,0 +1,38 @@
+# Alice and Bob Fall In Love
+# https://fivethirtyeight.com/features/alice-and-bob-fall-in-love/
+
+# Effort for 1 child of age n = 1/n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
